bdbe792eb1b431f3b4d5e241c2423983de0db0ab
jpecina <jpecina@gmail.com>
2026-02-10 00:47:36 +0100

    Database update

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,14 +1,19 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "1234",
-  database: process.env.DB_NAME || "shoponline",
-  port: Number(process.env.DB_PORT || 3306),
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const pool = mysql.createPool(
+  process.env.DATABASE_URL
+    ? process.env.DATABASE_URL
+    : {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: Number(process.env.DB_PORT || 3306),
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+      }
+);
 
 module.exports = pool;
+
